b87981120a37def8b30f57ec88c5a8e0cb3108e4
Leonardorossato <leonardorossato21@gmail.com>
2022-12-20 15:04:41 -0300

    user model ready

---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 250,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 250,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 250,
+    },
+    city: String,
+    state: String,
+    country: String,
+    occupation: String,
+    phoneNumber: String,
+    transactions: { type: Array, default: [] },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "usedr",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("users", UserSchema);
+export default User;
